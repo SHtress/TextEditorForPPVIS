6fe66faaa6d7454381446af9e30197383c847a27
SHtress <stas.butrin@mail.ru>
2020-04-25 17:05:47 +0300

    Backspace caret fix + added scroll bars

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -37,7 +38,7 @@ public class Main extends Application {
         primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                Canvas canvas = new Canvas(400,500);
+                Canvas canvas = new Canvas(410,800);
                 GraphicsContext gr=canvas.getGraphicsContext2D();
                 String key = event.getCode().toString();
                 if(key.equals(KeyCode.BACK_SPACE.toString()) && document.size()>0 && karetka.positionColumn!=1){
@@ -63,23 +64,30 @@ public class Main extends Application {
                 if(key.equals(KeyCode.DOWN.toString())) {if(karetka.positionRow<=document.size()/56 && karetka.positionColumn<=document.size()%56+1) karetka.positionRow++; }
 
                 //System.out.println(key);
-
                 int j=0,k=1;
                 for(int i=0;i<document.size();i++,k++) {
                     gr.setFont(document.get(i).SymbolFont);
-                    if( k*7>=393) {if(document.get(i).SymbolToRepresent.equals("\n") && i==karetka.positionRow*56) document.remove(i);
+                    if( k*7>=393) {
+                       if(document.get(i-3).SymbolToRepresent.equals("\n") && key.equals(KeyCode.BACK_SPACE.toString()) ){
+                        document.add(i-2,new Symbol("\n"));
+                       // System.out.println("GOOF");
+                        }
                     j++;  k=1; ;}
                     gr.fillText(document.get(i).SymbolToRepresent, k * 7, (j+1)*10);
                 }
 
                 gr.fillText("_",karetka.positionColumn*7,karetka.positionRow*10);
 
-                StackPane textpane = new StackPane(canvas);
+                ScrollPane scrollPane = new ScrollPane(canvas);
+                scrollPane.setPrefViewportHeight(500);
+                scrollPane.setPrefViewportWidth(410);
+                scrollPane.setPannable(false);
+                scrollPane.setVvalue((karetka.positionRow-1)*0.02);
+                scrollPane.setHvalue(0);
 
-                textpane.setMaxSize(400,500);
-                textpane.setPrefSize(400,500);
-                textpane.setMinSize(400,500);
-                textpane.setLayoutX(150);
+                StackPane textpane = new StackPane(scrollPane);
+
+                textpane.setLayoutX(50);
                 textpane.setLayoutY(25);
 
                 textpane.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
@@ -94,7 +102,7 @@ public class Main extends Application {
         primaryStage.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                Canvas canvas = new Canvas(400,500);
+                Canvas canvas = new Canvas(410,800);
                 GraphicsContext gr=canvas.getGraphicsContext2D();
 
                 String key = event.getCharacter() ;
@@ -109,18 +117,26 @@ public class Main extends Application {
                 int j=0,k=1;
                 for(int i=0;i<document.size();i++,k++) {
                         gr.setFont(document.get(i).SymbolFont);
-                        if( k*7>=393) { if(document.get(i-1).SymbolToRepresent.equals("\n") && i==karetka.positionRow*56) document.remove(i);
+                        if( k*7>=393) { if(document.get(i-1).SymbolToRepresent.equals("\n") && i==karetka.positionRow*56 && !key.toString().equals("\b")) document.remove(i);
                             j++;  k=1; }
                         gr.fillText(document.get(i).SymbolToRepresent, k * 7, (j+1)*10);
                     }
                 gr.fillText("_",karetka.positionColumn*7,karetka.positionRow*10);
 
-                StackPane textpane = new StackPane(canvas);
+                ScrollPane scrollPane = new ScrollPane(canvas);
+                scrollPane.setPrefViewportHeight(500);
+                scrollPane.setPrefViewportWidth(410);
+                scrollPane.setPannable(false);
+                scrollPane.setVvalue((karetka.positionRow-1)*0.02);
+                scrollPane.setHvalue(0);
 
-                textpane.setMaxSize(400,500);
-                textpane.setPrefSize(400,500);
-                textpane.setMinSize(400,500);
-                textpane.setLayoutX(150);
+                StackPane textpane = new StackPane(scrollPane);
+               // StackPane textpane = new StackPane(canvas);
+
+//                textpane.setMaxSize(400,500);
+//                textpane.setPrefSize(400,500);
+//                textpane.setMinSize(400,500);
+                textpane.setLayoutX(50);
                 textpane.setLayoutY(25);
 
                 textpane.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
@@ -133,7 +149,7 @@ public class Main extends Application {
 
 
 
-        Canvas canvas = new Canvas(400,500);
+        Canvas canvas = new Canvas(410,800);
         GraphicsContext gr=canvas.getGraphicsContext2D();
 //        int j=0,k=1;
 //        for(int i=0;i<document.size();i++,k++) {
@@ -142,12 +158,15 @@ public class Main extends Application {
 //            gr.fillText(document.get(i).SymbolToRepresent, k * 7, (j+1)*10);
 //        }
 
-        StackPane textpane = new StackPane(canvas);
+        ScrollPane scrollPane = new ScrollPane(canvas);
+        scrollPane.setPrefViewportHeight(500);
+        scrollPane.setPrefViewportWidth(410);
+        scrollPane.setPannable(false);
+        scrollPane.setVvalue(0.0);
+        scrollPane.setHvalue(0.0);
 
-        textpane.setMaxSize(400,500);
-        textpane.setPrefSize(400,500);
-        textpane.setMinSize(400,500);
-        textpane.setLayoutX(150);
+        StackPane textpane = new StackPane(scrollPane);
+        textpane.setLayoutX(50);
         textpane.setLayoutY(25);
 
         textpane.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
